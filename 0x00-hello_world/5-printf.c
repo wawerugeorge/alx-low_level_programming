1a27b3d83b7b696d046006f44076dbc3f2b8d27d
george <wawerugeorge390@gmail.com>
2023-03-21 00:36:50 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#incle < stdio.h >
+/**
+ * main -print a line using print f
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return(0);
+}
